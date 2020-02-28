00f1727ad9034693445c6a8a23841c09ac6d3170
Serhii Viazkov <s.viazkov@mmdsmart.com>
2020-02-28 11:02:23 +0200

    service solution

--- routers/pages.js
+++ routers/pages.js
@@ -1,45 +1,37 @@
 const express = require('express');
+const PageService = require('../services/PageService');
 const pages_router = express.Router();
-let data = require("../data/data");
-const {v4: uuidv4} = require('uuid');
-const fs = require("fs");
+let data = require('../data');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
 // method: GET, path: /pages
 pages_router.get('/', (req, res) => {
-    res.json(Object.values(data));
+  res.json(Object.values(data));
 });
 
 pages_router.get('/:page_id', (req, res) => {
-    try {
-        const page_id = data[req.params.page_id];
-        if (page_id >= 0 || page_id <= data) {
-            res.status(200).send(page_id);
-        } else {
-            res.status(400).send('Error: no exist such id')
-        }
-    } catch (e) {
-        console.log(e);
-    }
+  let page;
+  try {
+    page = PageService.readPage()
+  } catch (error) {
+    //TODO: реализовать корректную обработку ошибок
+    console.log(error);
+    res.error(400)
+  }
+  return res.send(page)
 });
 
 pages_router.post('/page', (res, req) => {
-    try {
-        const id = uuidv4();
-        const message = {
-            id,
-            text: req.body.text,
-        };
-        data[id] = message;
-        console.log(message);
-        fs.appendFile("./data.js", JSON.stringify(message), (error) => {
-            if (error) throw error; // если возникла ошибка
-            console.log("Асинхронная запись файла завершена. Содержимое файла:");
-            let data = fs.readFileSync("./data.js", "utf8");
-            console.log(data);  // выводим считанные данные
-        });
-    }catch (e) {
-        console.log(e);
-    }
+  let newPage;
+  try {
+    newPage = PageService.createPage()
+  } catch (error) {
+    //TODO: реализовать корректную обработку ошибок
+    console.log(error);
+    res.error(400)
+  }
+  return res.send(newPage)
 });
 
 module.exports = pages_router;
---
+++ services/PageService.js
@@ -0,0 +1,24 @@
+const data = require('../data');
+
+const createPage = (params) => {
+//TODO: а вот тут уже непосредственоо читаешь из базы / файла / переменной
+};
+const updatePage = (id, params) => {
+
+};
+const deletePage = (id) => {
+
+};
+const readPage = (id) => {
+
+};
+const readPages = () => {
+
+};
+
+module.exports.createPage = createPage;
+module.exports.updatePage = updatePage;
+module.exports.deletePage = deletePage;
+module.exports.readPage = readPage;
+module.exports.readPages = readPages;
+
