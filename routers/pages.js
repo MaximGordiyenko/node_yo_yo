97e980ac2b6aca52d44f3b37c622bcf0f6a70688
gmo88 <maxim.gordiyenko@gmail.com>
2020-03-06 21:51:32 +0200

    added id to the current data

--- routers/pages.js
+++ routers/pages.js
@@ -1,49 +1,43 @@
 const express = require('express');
 const PageService = require('../services/PageService');
 const pages_router = express.Router();
-// let data = require('../data');
-// const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 // const fs = require('fs');
 // const url = require('url');
-
+let id = uuidv4();
 
 // method: GET, path: /pages
 pages_router.get('/', (req, res) => {
-  try {
-    const pages = PageService.readPages();
-    return res.json(pages)
-  } catch (error) {
-    res.status(500);
-    console.log('Error while reading pages', error);
-    res.json(error)
-  }
+    try {
+        const pages = PageService.readPages();
+        res.json(pages)
+    } catch (error) {
+        console.log('Error while reading pages', error);
+        res.json(error);
+        res.status(500);
+    }
 });
 
 pages_router.get('/:page_id', (req, res) => {
-  const id = +req.params.page_id;
-  try {
-    return res.send(PageService.readPage(id));
-  } catch (error) {
-    res.status(500);
-    console.log(`Error while reading page by ${id}`, error);
-    res.json(error)
-  }
+    const id = parseInt(req.params.page_id);
+    try {
+        res.send(PageService.readPage(id));
+    } catch (error) {
+        res.status(500);
+        console.log(`Error while reading page by ${id}`, error);
+        res.json(error)
+    }
 });
 
 pages_router.post('/page', (req, res) => {
-  // let newPage;
-  try {
-      let reqData = {name: req.body.name, pass: req.body.pass, text: req.body.text};
-      console.log({id: 10, reqData});
-      return res.send(PageService.createPage(reqData));
-      // res.end();
-    // newPage = PageService.createPage()
-  } catch (error) {
-    //TODO: реализовать корректную обработку ошибок
-    console.log(error);
-    res.error(400)
-  }
-  // return res.send(newPage)
+    try {
+        let reqData = {[id]: {name: req.body.name, pass: req.body.pass, text: req.body.text}};
+        res.send(PageService.createPage(reqData));
+    } catch (error) {
+        //TODO: реализовать корректную обработку ошибок
+        console.log(error);
+        res.status(400).send();
+    }
 });
 
 module.exports = pages_router;
